03fcda11ebe19d28fad2a1f48ce2968393c05533
Brainiac-BV <k.dgray@23gmail.com>
2017-05-14 17:37:46 -0400

    added dynamic routing, more feeds

--- headlines.py
+++ headlines.py
@@ -1,11 +1,40 @@
+import feedparser
 from flask import Flask
 
+# app instantiation
 app = Flask(__name__)
 
+# set feed dictionary
+RSS_FEED = {'deadspin': 'http://deadspin.com/rss',
+            'cnn': 'http://rss.cnn.com/rss/edition.rss',
+            'fox': 'http://feeds.foxnews.com/foxnews/latest',
+            'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+# static routing implementation. would have to create one for each key in RSS_FEED
+# @app.route('/')
+# @app.route('/deadspin')
+# def deadspin():
+#    return get_news('deadspin')
+
+
+# primary view function, implementing dynamic routing
 @app.route('/')
-def index():
-    return "Headlines somethingor other"
+@app.route('/<publication>')
+# remember to set default value for variable passed from url
+def get_news(publication='deadspin'):
+    feed = feedparser.parse(RSS_FEED[publication])
+    first_article = feed['entries'][2]
+    return '''<html>
+    <body>
+        <h1> Headlines </h1>
+        <b>{0}</b><br/>
+        <i>{1}</i><br/>
+        <p>{2}</p><br/>
+    </body>
+</html>'''.format(first_article.get('title'),
+                  first_article.get('published'),
+                  first_article.get('summary'))
 
 
 if __name__ == "__main__":
-    app.run(port=5000,debug=True)
+    app.run(port=5000, debug=True)
